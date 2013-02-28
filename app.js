20da2ee30b83109916158d8c0fc33c97db2e8016
Marcus <marcus.h.olsson@gmail.com>
2013-02-28 16:06:42 +0100

    pars status

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var config = require('./config');
 var tc = tcHelper(config.hostname, config.port, config.user, config.password);
 
 var handleStatus = function(data){
-    console.log(data);
+    
+    var lastbuild = JSON.parse(data).build[0];
+    
+    if(lastbuild.status == "SUCCESS" && lastbuild.running){
+       console.log('building'); 
+    }else if(lastbuild.status == "FAILED"){
+         console.log('faild'); 
+    }else if(lastbuild.status == "SUCCESS"){
+         console.log('success'); 
+    };
+   
 }
 tc.getStatus(handleStatus);
