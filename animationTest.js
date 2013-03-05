2471c1bfad0ba2457d4427f5d57abb17066c006a
Marcus <marcus.h.olsson@gmail.com>
2013-03-05 09:59:23 +0100

    added time check for standup and coffe

--- animationTest.js
+++ animationTest.js
@@ -4,11 +4,11 @@ var animations = require('./animations/animations');
 var numberOfLEDs = 64;
 var lights = new LightStrips('/dev/spidev0.0', numberOfLEDs);
 
+lights.off()
 //lights.all(0, 0, 20);
 //lights.sync();
 //lights.off();
 //var throb = new animations.Throb(lights, numberOfLEDs, [255, 0, 0], [0, 0, 255], 5);
 //throb.start();
-
-var larsonScanner = new animations.LarsonScanner(lights, numberOfLEDs, [255, 0, 0], 20, 0.75, 0, 0, 5);
-larsonScanner.start();
+//var larsonScanner = new animations.LarsonScanner(lights, numberOfLEDs, [0, 0, 255], 10, 0.75, 0, 0, 1);
+//larsonScanner.start();
--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var tcHelper = require('./tcStatusGetter');
 var config = require('./config'); 
 var LightStrips = require('./LPD8806').LightStrips;
+var animations = require('./animations/animations');
 
 var numberOfLEDs = 64;
 var lights = new LightStrips('/dev/spidev0.0', numberOfLEDs);
@@ -47,5 +48,31 @@ var handleStatus = function(data){
 };
 
 off();
-setInterval(tc.getStatus, 1000, handleStatus);
+var standup = /^11:13/;
+var coffe = /^15:13/;
+var runningAnimation;
 
+var interval = function(){
+    var time = new Date().toTimeString();
+    
+    if(runningAnimation){
+        runningAnimation.stop()
+        runningAnimation = null;
+    }
+    
+    if(standup.test(time)){
+        runningAnimation = new animations.Throb(lights, numberOfLEDs, [255, 0, 0], [0, 0, 255], 5);
+        runningAnimation.start();
+        setTimeout(interval, 120000);
+    }else if(coffe.test(time)){
+        runningAnimation = new animations.LarsonScanner(lights, numberOfLEDs, [0, 0, 255], 10, 0.75, 0, 0, 1);
+        runningAnimation.start();
+        setTimeout(interval, 120000);
+    }
+    else{
+      tc.getStatus(handleStatus); 
+      setTimeout(interval, 1000);  
+    }
+}
+
+interval();
